5c99939a8b1fa7f478fec2497282c4a34d7d3260
PK <krzysztofpielot@gmail.com>
2026-01-03 21:08:32 +0100

    create logn/register site, pre-css, create core app, changed place of templates

---
+++ lensy_backend/core/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def home_view(request):
+    return render(request, "core/home.html")
