d61423ca5ec9873fc5b231e6620eb62ccbc6333d
PK <krzysztofpielot@gmail.com>
2026-01-05 22:00:22 +0100

    Register posts at admin and changes in views

--- lensy_backend/posts/views.py
+++ lensy_backend/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile_view(request):
+    profile_user = request.user
+    posts = profile_user.posts.all().order_by('-created_at')
+
+    return render(request, 'accounts/profile.html', {
+        'profile_user': profile_user,
+        'posts': posts
+    })
