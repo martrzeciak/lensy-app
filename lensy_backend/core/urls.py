c1650705057596facd7ac07f109427870bdba6b1
PK <krzysztofpielot@gmail.com>
2026-01-18 20:36:06 +0100

    Function, urla and templates for main site

--- lensy_backend/core/urls.py
+++ lensy_backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home_view
+from . import views
 
 urlpatterns = [
-    path("", home_view, name="home"),
+    path("", views.home_view, name="home"),
 ]
--- lensy_backend/core/views.py
+++ lensy_backend/core/views.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from posts.models import Post
+from accounts.models import Follow
+
 
 @login_required
 def home_view(request):
-    return render(request, "core/home.html")
+    following_ids = set(
+        Follow.objects.filter(
+            follower=request.user
+        ).values_list('following_id', flat=True)
+    )
+
+    posts = Post.objects.filter(
+        author_id__in=following_ids
+    ).select_related('author').order_by('-created_at')
+
+    return render(request, 'core/home.html', {'posts': posts})
+
