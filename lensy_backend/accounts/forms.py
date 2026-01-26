5f56d484e81a7063eb4cad21001d0eb3685efea4
martrzeciak <mariusz.trzeciak09@gmail.com>
2026-01-26 23:07:08 +0100

    Enhance user interface and improve accessibility across account-related templates

--- lensy_backend/accounts/forms.py
+++ lensy_backend/accounts/forms.py
@@ -6,8 +6,17 @@ class EditProfileForm(forms.ModelForm):
         model = CustomUser
         fields = ['username', 'first_name', 'last_name', 'bio']
         widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
             'bio': forms.Textarea(attrs={
-                'rows': 2,
+                'rows': 4,
                 'class': 'form-control',
                 'placeholder': 'Napisz coś o sobie...'
             }),
